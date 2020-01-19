f9f38d55e50a52aaec207bb758fdc42c316f0f08
‘gonzo’ <pnw123456@icloud.com>
2020-01-19 10:25:20 +0900

    spring boot setting - gradle #2

--- api/src/main/java/com/app/api/statics/user/User.java
+++ api/src/main/java/com/app/api/statics/user/User.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -17,8 +14,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "test_user")
 public class User {
-     @Id
-     private Long seq;
-     @Column(name = "title")
-     private String title;
+    @Id
+    // auto seq
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long seq;
+    @Column(name = "title")
+    private String title;
 }
