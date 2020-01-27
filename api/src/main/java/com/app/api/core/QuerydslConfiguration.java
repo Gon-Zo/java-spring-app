d65917f9815d7cb553e436b70d71a891128a54d7
gonzo <m165252@daum.net>
2020-01-27 10:05:03 +0900

    use to service #6

--- api/src/main/java/com/app/api/core/QuerydslConfiguration.java
+++ api/src/main/java/com/app/api/core/QuerydslConfiguration.java
@@ -1,4 +1,4 @@
-package com.app.api.conf;
+package com.app.api.core;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
--- api/src/main/java/com/app/api/service/posts/PostsServiceImpl.java
+++ api/src/main/java/com/app/api/service/posts/PostsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.app.api.service.posts;
 
-import com.app.api.domain.posts.Posts;
 import com.app.api.domain.posts.PostsRepository;
 import com.app.api.domain.posts.support.PostsRepositorySupport;
 import com.app.api.web.dto.PostsResponseDto;
@@ -25,8 +24,9 @@ public class PostsServiceImpl implements PostsService {
     @Override
     @Transactional
     public Long update(Long id, PostsResponseDto responseDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Not id >> " + id));
+//        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Not id >> " + id));
 //        posts.update(responseDto.getTitle() , responseDto.getContent());
+        postsRepositorySupport.update(id, responseDto);
         return id;
     }
 
