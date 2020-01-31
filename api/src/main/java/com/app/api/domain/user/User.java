359d38f85ad73d18eed2c42c942c75f115ca259d
gonzo <m165252@daum.net>
2020-01-31 20:52:08 +0900

    @setter not use to class #8

--- api/src/main/java/com/app/api/domain/user/User.java
+++ api/src/main/java/com/app/api/domain/user/User.java
@@ -2,17 +2,15 @@ package com.app.api.domain.user;
 
 import com.app.api.core.BaseEntity;
 import lombok.Data;
-import lombok.Getter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-//@Getter
 @Entity
 @Data
 // todo : table name change - jkpark
-@Table(name = "mk_user_2")
+@Table(name = "app_user")
 public class User extends BaseEntity {
 
     @Column(name = "email")
