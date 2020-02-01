95d3267950ee3ae3ddeb74ee2f25ffa6833d6340
‘gonzo’ <pnw123456@icloud.com>
2020-02-01 12:27:31 +0900

    gradle build test issue #9

--- api/src/test/java/com/app/api/web/HelloControllerTest.java
+++ api/src/test/java/com/app/api/web/HelloControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
