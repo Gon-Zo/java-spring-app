b46d853e54d9a8d9ce325fb77b734d2ea709956d
gonzo <m165252@daum.net>
2020-03-20 23:54:48 +0900

    vue watch dev #13

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -3,22 +3,24 @@ import Router from 'vue-router'
 import blog from "../components/blog/blog";
 import user from "../components/user/user";
 
-Vue.use(Router)
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// import Vuex from 'vuex'
-// Vue.use(Vuex)
+Vue.use(Router);
+Vue.use(BootstrapVue);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'user',
-      component: user
-    },
-    {
-      path: '/blog',
       name: 'blog',
       component: blog
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: user
     }
   ]
 })
