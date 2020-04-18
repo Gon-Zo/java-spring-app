48434ba552d9076d96413cd2c6dfffbc22484f32
‘gonzo’ <pnw123456@icloud.com>
2020-04-18 12:34:24 +0900

    #00 크로스 오리진 설정

---
+++ api/src/main/java/com/app/api/core/WebMvcConfigurerImpl.java
@@ -0,0 +1,25 @@
+package com.app.api.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebMvcConfigurerImpl implements WebMvcConfigurer {
+
+
+    /**
+     * crose orgin setting
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080");
+    }
+
+
+}
