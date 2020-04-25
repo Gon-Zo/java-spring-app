4ecb2e06db66e0e23ec4fbeb6be38cbdf5832348
‘gonzo’ <pnw123456@icloud.com>
2020-04-25 20:38:02 +0900

    #001 spring security

--- api/src/main/java/com/app/api/domain/user/UserRepository.java
+++ api/src/main/java/com/app/api/domain/user/UserRepository.java
@@ -1,6 +1,5 @@
 package com.app.api.domain.user;
 
-import com.app.api.demo.auth.CustomUserDetails;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
