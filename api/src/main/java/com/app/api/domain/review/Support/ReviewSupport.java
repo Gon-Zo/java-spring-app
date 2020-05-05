10c2c09e1ffdefdfe5d14de6fdd54bed614afcf0
‘gonzo’ <pnw123456@icloud.com>
2020-05-05 19:30:44 +0900

    #010 review api develop

--- api/src/main/java/com/app/api/domain/review/Support/ReviewSupport.java
+++ api/src/main/java/com/app/api/domain/review/Support/ReviewSupport.java
@@ -2,8 +2,11 @@ package com.app.api.domain.review.Support;
 
 import com.app.api.domain.review.Review;
 import com.app.api.web.dto.PageableDto;
+import com.app.api.web.dto.ReviewResponseDto;
 import org.springframework.data.domain.Page;
 
 public interface ReviewSupport {
-    Page<Review> findByReview (PageableDto dto);
+    Page<Review> findByReview(PageableDto dto);
+
+    void update(ReviewResponseDto dto);
 }
--- api/src/main/java/com/app/api/service/reivew/ReviewServiceImpl.java
+++ api/src/main/java/com/app/api/service/reivew/ReviewServiceImpl.java
@@ -1,25 +1,92 @@
 package com.app.api.service.reivew;
 
+import com.app.api.core.error.exception.BusinessException;
+import com.app.api.core.error.exception.ErrorCode;
+import com.app.api.domain.image.ImageRepository;
+import com.app.api.domain.product.Product;
+import com.app.api.domain.product.ProductRepository;
 import com.app.api.domain.review.Review;
 import com.app.api.domain.review.ReviewRepository;
 import com.app.api.domain.review.Support.ReviewSupport;
+import com.app.api.domain.user.User;
+import com.app.api.domain.user.UserRepository;
+import com.app.api.enums.ImageType;
+import com.app.api.web.dto.ImageResponseDto;
 import com.app.api.web.dto.PageableDto;
+import com.app.api.web.dto.ReviewResponseDto;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
-@Service @AllArgsConstructor
-public class ReviewServiceImpl implements ReviewService {
+import javax.transaction.Transactional;
 
+import static com.app.api.utils.ApiDomainUtils.isNotEmpty;
+
+@Service
+@AllArgsConstructor
+public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewSupport support;
 
     private final ReviewRepository repository;
 
+    private final UserRepository userRepository;
+
+    private final ProductRepository productRepository;
+
+    private final ImageRepository imageRepository;
 
     @Override
     public Page<Review> getPageReview(PageableDto dto) {
-
-        return null;
+        return support.findByReview(dto);
     }
+
+    @Override
+    public void createReview(ReviewResponseDto dto) {
+
+        User user = userRepository
+                .findById(dto.getUserSeq())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        Product product = productRepository
+                .findById(dto.getProductSeq())
+                .orElseThrow(() -> new BusinessException(ErrorCode.PRODUCT_NOT_FOUND));
+
+        dto.setUser(user);
+
+        dto.setProduct(product);
+
+        Review review = repository.saveAndFlush(dto.toEntity());
+
+        Long seq = review.getSeq();
+
+        if (isNotEmpty(dto.getImageVals())) {
+
+            dto.getImageVals().stream().forEach(f ->
+                            imageRepository.save(
+                                    ImageResponseDto.builder()
+                                            .dataId(seq)
+                                            .photo(f)
+                                            .type(ImageType.R.getValue())
+                                            .build()
+                                            .toEntity()
+                            )
+                    );
+
+        }
+
+    }
+
+    @Override
+    @Transactional
+    public void removeReview(long seq) {
+        repository.deleteById(seq);
+    }
+
+    @Override
+    @Transactional
+    public void updateReivew(ReviewResponseDto dto) {
+
+    }
+
 }
