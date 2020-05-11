f4b20712aab02288550fc517a032fdb20cbd7cb7
‘gonzo’ <pnw123456@icloud.com>
2020-05-11 19:57:20 +0900

    #015 Auth Role Change

--- api/src/main/java/com/app/api/web/dto/MenuResponseDto.java
+++ api/src/main/java/com/app/api/web/dto/MenuResponseDto.java
@@ -1,9 +1,13 @@
 package com.app.api.web.dto;
 
 import com.app.api.domain.menu.Menu;
+import com.app.api.domain.url.Url;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+
+import java.util.List;
 
 @ToString
 @Getter
@@ -18,12 +22,17 @@ public class MenuResponseDto {
 
     private Long menuOrder;
 
+    private List<Long> urlSeq;
+
+    @Setter private List<Url> authUrl;
+
     public Menu toEntity(){
        return  Menu.builder()
                .title(title)
                .url(url)
                .icon(icon)
                .menuOrder(menuOrder)
+               .authUrl(authUrl)
                .build();
     }
 
