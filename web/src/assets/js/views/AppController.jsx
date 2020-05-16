553e6f7575eece945479cb6e594c34f5eee2f555
‘gonzo’ <pnw123456@icloud.com>
2020-05-16 10:03:16 +0900

    #205 dev controller api

--- web/src/assets/js/views/AppController.jsx
+++ web/src/assets/js/views/AppController.jsx
@@ -3,18 +3,17 @@ import {Button, Tab, Tabs} from 'react-bootstrap'
 import {$httpGetMenus, $httpGetRoles, $httpGetUrls, $httpGetUsers} from "../modules/api/controller";
 import {useDispatch, useSelector} from "react-redux";
 import Table from "../components/app/Table";
-import {_bindData} from "../modules/static/support";
 import Pagination from "../components/app/Pagination";
 
 export default () => {
 
     let [key, setKey] = useState('Menus');
 
-    let [payload, setPayload] = useState([])
-
     let dispatch = useDispatch()
 
     let initData = useSelector(state => state.controllerReducer, [])
+
+    let payload = initData.payload
 
     let _selectKey = (k) => {
         switch (k) {
@@ -34,13 +33,6 @@ export default () => {
         setKey(k)
     }
 
-    // useEffect(()=>{
-    //     $httpGetMenus(dispatch)
-    //     return () =>{
-    //         console.log("TEST SUCCESS")
-    //     }
-    // } , [])
-
     useEffect(() => {
         $httpGetMenus(dispatch)
     }, []);
@@ -52,6 +44,31 @@ export default () => {
     let _onDelete = () => {
     }
 
+
+    function RenderingTable(props) {
+        let payload = props.data
+
+        return(
+            <div  className="container-main">
+
+                <Table data={payload.data}
+                       keys={payload.key}
+                       update={_onEdit}
+                       delete={_onDelete}
+                />
+
+                <Pagination count={payload.count}
+                            numPages={payload.numPages}
+                            showPages={payload.showPages}
+                            refresh={_onReFresh}
+                    // page={initUser.page}
+                            page={1}
+                />
+
+            </div>
+        )
+    }
+
     return (
         <div>
             <Tabs defaultActiveKey={key}
@@ -59,41 +76,11 @@ export default () => {
                   onSelect={(k) => _selectKey(k)}>
 
                 <Tab eventKey="Menus" title="Menus">
-                    <div  className="container-main">
-
-                        <Table data={_bindData(initData.menus, filterBy(key)).data}
-                               keys={_bindData(initData.menus, filterBy(key)).key}
-                               update={_onEdit}
-                               delete={_onDelete}
-                        />
-
-                        <Pagination count={_bindData(initData.menus, filterBy(key)).count}
-                                    numPages={_bindData(initData.menus, filterBy(key)).numPages}
-                                    showPages={_bindData(initData.menus, filterBy(key)).showPages}
-                                    refresh={_onReFresh}
-                                    // page={initUser.page}
-                                    page={1}
-                        />
-
-                    </div>
+                    <RenderingTable data={payload}/>
                 </Tab>
 
                 <Tab eventKey="Users" title="Users">
-                    <div  className="container-main">
-
-                        <Table data={_bindData(initData.users, filterBy(key)).data}
-                               keys={_bindData(initData.users, filterBy(key)).key}
-                               update={_onEdit}
-                               delete={_onDelete}
-                        />
-                        <Pagination count={_bindData(initData.users, filterBy(key)).count}
-                                    numPages={_bindData(initData.users, filterBy(key)).numPages}
-                                    showPages={_bindData(initData.users, filterBy(key)).showPages}
-                                    refresh={_onReFresh}
-                                    page={1}
-                        />
-
-                    </div>
+                    <RenderingTable data={payload}/>
                 </Tab>
 
 
@@ -114,29 +101,4 @@ export default () => {
 
     )
 }
-
-
-let filterBy = (k) => {
-    let filter;
-    switch (k) {
-        case "Menus":
-            filter = [
-                {key: 'title', name: '메뉴명'},
-                {key: 'url', name: "주소"},
-                {key: 'icon', name: "아이콘"}
-            ]
-            break;
-        case "Users":
-            filter = [
-                {key: 'email', name: "이메일"},
-                {key: 'birthDate', name: "생년월일"},
-                {key: 'address', name: "주소"},
-                {key: 'isUse', name: "상태"},
-                {key: 'createdAt', name: "등록일"}
-            ]
-            break;
-    }
-    return filter;
-}
-
 
