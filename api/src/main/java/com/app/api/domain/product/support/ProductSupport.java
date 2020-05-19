348341a46032c7fa65ada4fc43fd4b2044e93332
gonzo <m165252@daum.net>
2020-05-19 20:31:32 +0900

    #018 DEV Product api

--- api/src/main/java/com/app/api/domain/product/support/ProductSupport.java
+++ api/src/main/java/com/app/api/domain/product/support/ProductSupport.java
@@ -3,12 +3,17 @@ package com.app.api.domain.product.support;
 import com.app.api.domain.product.Product;
 import com.app.api.web.dto.PageableDto;
 import com.app.api.web.dto.ProductResponseDto;
+import com.querydsl.core.Tuple;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface ProductSupport {
 
     void update(long seq , ProductResponseDto dto);
 
     Page<Product> findByProducts (PageableDto dto);
+
+    List<Tuple> findGroupByTitle();
 
 }
