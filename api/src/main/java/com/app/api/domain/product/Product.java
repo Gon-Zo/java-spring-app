92842715c034f20894db9b1843aa56eb0a7d3758
‘gonzo’ <pnw123456@icloud.com>
2020-05-19 19:38:16 +0900

    #019 dev product api

--- api/src/main/java/com/app/api/domain/product/Product.java
+++ api/src/main/java/com/app/api/domain/product/Product.java
@@ -17,37 +17,35 @@ import java.util.List;
 @Table(name = "PRODUCTS")
 public class Product extends BaseEntity {
 
-    @Column(name = "title")
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "cnt")
+    @Column(name = "cnt", nullable = false)
     private Integer cnt;
 
-    @Column(name = "price")
+    @Column(name = "price", nullable = false)
     private BigDecimal price;
 
-    @Column(name = "info" , columnDefinition = "TEXT")
+    @Column(name = "info", columnDefinition = "TEXT", nullable = false)
     private String info;
 
-    @Column(name = "isSold")
+    @Column(name = "is_sold", nullable = false)
     private Boolean isSold;
 
     @ManyToOne
-    @JoinColumn(name = "store_seq")
+    @JoinColumn(name = "store_seq", nullable = false)
     private Store store;
 
     @OneToMany
     private List<Image> images;
 
-
     @Builder
-    public Product(String title , Integer cnt , BigDecimal price , String info , Boolean isSold) {
-       this.title = title;
-       this.cnt = cnt;
-       this.price = price;
-       this.info = info;
-       this.isSold = isSold;
+    public Product(String title, Integer cnt, BigDecimal price, String info, Boolean isSold) {
+        this.title = title;
+        this.cnt = cnt;
+        this.price = price;
+        this.info = info;
+        this.isSold = isSold;
     }
-
 
 }
