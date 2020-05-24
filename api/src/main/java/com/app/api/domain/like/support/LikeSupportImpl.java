70a3f51b28e1e9f6563e614a5dc315560163c87e
‘gonzo’ <pnw123456@icloud.com>
2020-05-24 17:59:35 +0900

    #020 dev client api

--- api/src/main/java/com/app/api/domain/like/support/LikeSupportImpl.java
+++ api/src/main/java/com/app/api/domain/like/support/LikeSupportImpl.java
@@ -1,16 +1,19 @@
 package com.app.api.domain.like.support;
 
 import com.app.api.domain.like.Like;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class LikeSupportImpl extends QuerydslRepositorySupport  implements LikeSupport {
 
-    private final JpaRepositoryFactory jpaRepositoryFactory;
+    private final JPAQueryFactory jpaQueryFactory;
 
-    public LikeSupportImpl( JpaRepositoryFactory jpaRepositoryFactory) {
+    public LikeSupportImpl(JPAQueryFactory jpaQueryFactory) {
         super(Like.class);
-        this.jpaRepositoryFactory = jpaRepositoryFactory;
+        this.jpaQueryFactory = jpaQueryFactory;
     }
 
 }
