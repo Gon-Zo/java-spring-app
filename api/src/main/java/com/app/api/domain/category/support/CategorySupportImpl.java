139e9cdad30913666543618e54d27e870be80d8b
gonzo <m165252@daum.net>
2020-05-26 20:23:54 +0900

    #021 Manager Api update by category

--- api/src/main/java/com/app/api/domain/category/support/CategorySupportImpl.java
+++ api/src/main/java/com/app/api/domain/category/support/CategorySupportImpl.java
@@ -2,11 +2,20 @@ package com.app.api.domain.category.support;
 
 import com.app.api.domain.category.Category;
 import com.app.api.web.dto.CategoryResponseDto;
+import com.app.api.web.dto.PageableDto;
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.dml.UpdateClause;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.JPQLQueryFactory;
 import com.querydsl.jpa.impl.JPAUpdateClause;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import static com.app.api.domain.category.QCategory.category;
 import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
@@ -35,6 +44,25 @@ public class CategorySupportImpl extends QuerydslRepositorySupport implements Ca
         }
 
         update.where(category.seq.eq(seq)).execute();
+
+    }
+
+    @Override
+    public Page<Category> findByPage(PageableDto dto) {
+
+        JPQLQuery<Category> query = jpqlQueryFactory.selectFrom(category);
+
+        PageRequest pageable = PageRequest.of(dto.getPage(), dto.getSize());
+
+        query.limit(pageable.getPageSize()).offset(pageable.getOffset());
+
+        QueryResults<Category> fetchResults = query.fetchResults();
+
+        long total = fetchResults.getTotal();
+
+        List<Category> results = fetchResults.getResults();
+
+        return new PageImpl<>(results , pageable , total);
 
     }
 
--- api/src/main/java/com/app/api/service/category/CategoryService.java
+++ api/src/main/java/com/app/api/service/category/CategoryService.java
@@ -2,6 +2,8 @@ package com.app.api.service.category;
 
 import com.app.api.domain.category.Category;
 import com.app.api.web.dto.CategoryResponseDto;
+import com.app.api.web.dto.PageableDto;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -16,5 +18,7 @@ public interface CategoryService {
     void updateCategory(long seq , CategoryResponseDto dto);
 
     List<Category> getCategories();
+
+    Page<Category> getPages(PageableDto dto);
 
 }
