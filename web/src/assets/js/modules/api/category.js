e6c250812ebeaafb520e999c9535fae0ec103dbd
‘gonzo’ <pnw123456@icloud.com>
2020-05-26 20:33:18 +0900

    #208 manager category UI update

--- web/src/assets/js/modules/api/category.js
+++ web/src/assets/js/modules/api/category.js
@@ -1,28 +1,32 @@
 import axios from "axios";
 import {isOpenToCategory, setCategory} from "../reducer/category";
 
-export function $setUpCategory(dispatch) {
-    axios.get(`/admin/category`)
+export function $setCategory(dispatch) {
+    axios.get(`/manager/category` , {
+        page : 0 ,
+        size : 10 ,
+        sort : '---',
+    })
         .then((res) => dispatch(setCategory(res.data)))
         .catch(err => console.log(err))
 }
 
 export function $updateCategory(dispatch , payload) {
     let seq = payload.seq;
-    axios.put(`/admin/category${seq}` , payload)
+    axios.put(`/manager/category${seq}` , payload)
         .then((res)=>console.log(res.status))
         .catch(err=>console.log(err))
 }
 
 export function $deleteCategory(dispatch , payload) {
     let seq = payload.seq
-    axios.delete(`/admin/category${seq}`)
+    axios.delete(`/manager/category${seq}`)
         .then((res)=>console.log(res.status))
         .catch(err=>console.log(err))
 }
 
 export function $createCategory(dispatch, payload) {
-    axios.post(`/admin/category`)
+    axios.post(`/manager/category`)
         .then((res) => console.log(res.status))
         .catch(err => console.log(err))
 }
