e0c3f3f3ba8a5b772f1015cf18c5be25fb1571d2
‘gonzo’ <pnw123456@icloud.com>
2020-07-09 20:32:37 +0900

    #021 Notice api dev

--- api/src/main/java/com/app/api/service/notice/NoticeService.java
+++ api/src/main/java/com/app/api/service/notice/NoticeService.java
@@ -1,6 +1,9 @@
 package com.app.api.service.notice;
 
+import com.app.api.domain.notice.Notice;
 import com.app.api.web.dto.NoticeResponseDto;
+import com.app.api.web.dto.PageableDto;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +12,11 @@ public interface NoticeService {
     void createNotice(NoticeResponseDto dto);
 
     void updateNotice(NoticeResponseDto dto , long seq);
+
+    Notice getNotice (long seq);
+
+    Page<Notice> getNoticeList (PageableDto dto);
+
+    void removeNotice(long seq);
 
 }
