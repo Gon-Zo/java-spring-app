d9d5f036902d900ae67c9b9ff071fe41a7539610
‘gonzo’ <pnw123456@icloud.com>
2020-07-14 19:42:24 +0900

    #224 management card dev & home view dev

--- web/src/assets/components/views/Home.jsx
+++ web/src/assets/components/views/Home.jsx
@@ -2,8 +2,29 @@ import React from "react";
 
 export default () => {
     return (
-        <div>
-            Home
+        <div className="common-wrap home-content mt-5">
+            <div className="grid-items m-2">
+               <div className="grid-item">
+
+                   <div className="grid-item-title">
+                       <span>Today's Work</span>
+                   </div>
+
+                   <div className="grid-item-body">
+
+                   </div>
+
+
+               </div>
+            </div>
+            <div className="grid-items m-2">
+               <div className="grid-item">
+
+               </div>
+            </div>
+            <div className="grid-items m-2">3</div>
+            <div className="grid-items m-2">4</div>
+            <div className="grid-items m-2">5</div>
         </div>
     )
 }
