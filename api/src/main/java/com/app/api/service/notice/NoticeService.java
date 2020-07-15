ba1a34176355d1ae5850aa3c3fec9e2edd462532
‘gonzo’ <pnw123456@icloud.com>
2020-07-15 19:50:45 +0900

    #224 notice api dev

--- api/src/main/java/com/app/api/service/notice/NoticeService.java
+++ api/src/main/java/com/app/api/service/notice/NoticeService.java
@@ -4,7 +4,6 @@ import com.app.api.domain.notice.Notice;
 import com.app.api.web.dto.NoticeResponseDto;
 import com.app.api.web.dto.PageableDto;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
 
