f3d29db7cda87ca96c2383fa24b240dee1343e3b
gonzo <m165252@daum.net>
2020-07-15 20:03:30 +0900

    #224 code modify

--- api/src/main/java/com/app/api/web/client/ClientOrderController.java
+++ api/src/main/java/com/app/api/web/client/ClientOrderController.java
@@ -4,6 +4,8 @@ import com.app.api.service.order.OrderService;
 import com.app.api.web.dto.OrderResponseDto;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
+
 @RestController
 @RequestMapping("/client/order")
 public class ClientOrderController {
@@ -14,13 +16,23 @@ public class ClientOrderController {
         this.service = service;
     }
 
+    /**
+     * 주문 신청
+     *
+     * @param dto
+     */
     @PostMapping("")
-    public void orderedProduct(OrderResponseDto dto){
-       service.saveBy(dto);
+    public void makeOrder(OrderResponseDto dto) {
+        service.saveBy(dto);
     }
 
+    /**
+     * 주문 취소
+     *
+     * @param seq
+     */
     @DeleteMapping("/{seq}")
-    public void cancelOrder(@PathVariable long seq) {
+    public void cancelOrder(@PathVariable @NotNull long seq) {
         service.removeBy(seq);
     }
 
