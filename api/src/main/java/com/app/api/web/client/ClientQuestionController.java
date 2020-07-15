768a4d573ad90ac474799132eaa36e82a976a5ac
gonzo <m165252@daum.net>
2020-07-15 20:11:39 +0900

    #224 code modify

--- api/src/main/java/com/app/api/web/client/ClientQuestionController.java
+++ api/src/main/java/com/app/api/web/client/ClientQuestionController.java
@@ -7,6 +7,8 @@ import com.app.api.web.dto.QustionResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
+
 @RestController
 @RequestMapping("/client/question")
 public class ClientQuestionController {
@@ -28,17 +30,17 @@ public class ClientQuestionController {
     }
 
     @PutMapping("/{seq}")
-    public void reviseQuestion(@PathVariable long seq , QustionResponseDto dto){
+    public void reviseQuestion(@PathVariable @NotNull long seq , QustionResponseDto dto){
         service.updateQuestion(seq, dto);
     }
 
     @DeleteMapping("/{seq}")
-    public void removeQuestion(@PathVariable long seq){
+    public void removeQuestion(@PathVariable @NotNull long seq){
         service.removeQuestion(seq);
     }
 
     @GetMapping("/{seq}")
-    public Question showQuestion(@PathVariable long seq){
+    public Question showQuestion(@PathVariable @NotNull long seq){
         return service.getQuestion(seq);
     }
 
