0ff698fac6963565e6301ba33571c11e876f74bb
gonzo <m165252@daum.net>
2020-07-17 19:36:45 +0900

    #101 Code Update

--- api/src/main/java/com/app/api/core/error/exception/ErrorCode.java
+++ api/src/main/java/com/app/api/core/error/exception/ErrorCode.java
@@ -7,18 +7,8 @@ public enum ErrorCode {
 
     INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
     METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
-    ENTITY_NOT_FOUND(400, "C003", " Entity Not Found"),
-    INTERNAL_SERVER_ERROR(500, "C004", "Server Error"),
     INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
     HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
-
-    // Member
-    EMAIL_DUPLICATION(400, "M001", "Email is Duplication"),
-    LOGIN_INPUT_INVALID(400, "M002", "Login input is invalid"),
-
-    // Coupon
-    COUPON_ALREADY_USE(400, "CO001", "Coupon was already used"),
-    COUPON_EXPIRE(400, "CO002", "Coupon was already expired"),
 
     // Uesr
     LOGIN_USER_NOT_FOUND(500, "U001", "Login User Not Found"),
@@ -55,8 +45,6 @@ public enum ErrorCode {
     MENUN_NOT_FOUND(500 , "M001" , "Menu Not Found"),
 
     AUTHURL_NOT_FOUND(500 , "A001" , "AuthUrl Not Found"),
-
-    INIT_ROLE_ERROR( 500 , "I001" , "init to Role Error") ,
 
     REVIEW_NOT_FOUND(500 , "R001" , "Review Not Found"),
 
