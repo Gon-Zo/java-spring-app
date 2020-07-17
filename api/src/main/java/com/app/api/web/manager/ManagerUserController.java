8194a996870182b2907b9f75b17f16dd8bd3a909
gonzo <m165252@daum.net>
2020-07-17 19:05:49 +0900

    #224 user manager resetting

--- api/src/main/java/com/app/api/web/manager/ManagerUserController.java
+++ api/src/main/java/com/app/api/web/manager/ManagerUserController.java
@@ -19,23 +19,47 @@ public class ManagerUserController {
 
     private final UserService userService;
 
+    /**
+     * Update to User
+     *
+     * @param seq
+     * @param dto
+     * @return
+     */
     @PutMapping("/{seq}")
     public Long modifyInfo(@PathVariable long seq, @RequestBody UserRespoenseDto dto) {
         return userService.updateFrom(seq, dto);
     }
 
+    /**
+     * Remove User
+     *
+     * @param seq
+     */
     @DeleteMapping("/{seq}")
     public void removeUser(@PathVariable long seq) {
         userService.deleteByUser(seq);
     }
 
+    /**
+     * Show User Data
+     *
+     * @param seq
+     * @return
+     */
     @GetMapping("/{seq}")
     public User showUser(@PathVariable long seq) {
         return userService.getUser(seq);
     }
 
+    /**
+     * Show User List Data
+     *
+     * @param dto
+     * @return Page<User>
+     */
     @GetMapping("")
-    public Page<User> showPageUsers(PageableDto dto){
+    public Page<User> showPageUsers(PageableDto dto) {
         return userService.getUsers(Arrays.asList(Roles.U.getValue()), dto);
     }
 
